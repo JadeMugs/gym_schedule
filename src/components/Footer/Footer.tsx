151527ad79daca7d665b0350fef98fc94065a027
JadeMugs <giada.boccali@gmail.com>
2022-06-01 17:25:40 +0200

    Some footer style optimizations

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,29 +6,31 @@ type ComponentFooterProps = {};
 export const Footer: React.FC<ComponentFooterProps> = () => {
 	return (
 		<footer>
-			<a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-				<img src="/assets/instagram.png" alt="instagram" width="32px" />
-			</a>
-			<a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-				<img src="/assets/fb.png" alt="facebook" width="32px" />
-			</a>
-			<a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-				<img
-					src="/assets/youtube.png"
-					alt="youtube"
-					width="32px"
-					// height="32px"
-					style={{ borderRadius: "50%" }}
-				/>
-			</a>
-
 			<div>
-				<img src="/assets/italy.png" alt="italiano" width="24px" />
-				ITA <OpenAnnouncementIcon />
+				<a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+					<img src="/assets/instagram.png" alt="instagram" width="24px" />
+				</a>
+				<a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+					<img src="/assets/fb.png" alt="facebook" width="24px" />
+				</a>
+				<a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+					<img
+						src="/assets/youtube.png"
+						alt="youtube"
+						width="24px"
+						style={{ borderRadius: "50%" }}
+					/>
+				</a>
 			</div>
-			<p>
+
+			<div className="language">
+				<img src="/assets/italy.png" alt="italiano" width="18px" />
+				<p>ITA</p> <OpenAnnouncementIcon />
+			</div>
+
+			<small>
 				Made by <strong>JadeMugs</strong> for <strong>PED</strong>
-			</p>
+			</small>
 		</footer>
 	);
 };
